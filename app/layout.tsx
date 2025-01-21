fcc46ea362e2cd1d8080ce82bd28269869613437
SimplyRadAppsAdmin <admin@simplyradapps.com>
2025-01-20 22:21:42 -0800

    changed name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { ThemeSwitcher } from "@/components/theme-switcher";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
@@ -10,7 +9,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
+  title: "Simply Rad Apps",
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  // output: "export",
-  // reactStrictMode: true,
-  // basePath: "/simply-rad-apps",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
